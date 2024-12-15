306e344db2a066b61699e4cd82059c9619461d00
abuzarsince2002 <abuzarsince2002@gmail.com>
2024-12-15 10:09:35 +0530

    Initial Commit

---
+++ Emoji Dictionary/Emoji_DictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_DictionaryApp.swift
+//  Emoji Dictionary
+//
+//  Created by Abuzar Siddiqui on 15/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_DictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Emoji DictionaryTests/Emoji_DictionaryTests.swift
@@ -0,0 +1,16 @@
+//
+//  Emoji_DictionaryTests.swift
+//  Emoji DictionaryTests
+//
+//  Created by Abuzar Siddiqui on 15/12/24.
+//
+
+import Testing
+
+struct Emoji_DictionaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
