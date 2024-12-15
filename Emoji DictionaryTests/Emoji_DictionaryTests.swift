ab55fa8586fd08171c50a246da34a2d43934e08c
abuzarsince2002 <abuzarsince2002@gmail.com>
2024-12-15 13:13:39 +0530

    Initial Commit

---
+++ Emoji DictionaryTests/Emoji_DictionaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_DictionaryTests.swift
+//  Emoji DictionaryTests
+//
+//  Created by Abuzar Siddiqui on 15/12/24.
+//
+
+import Testing
+@testable import Emoji_Dictionary
+
+struct Emoji_DictionaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
