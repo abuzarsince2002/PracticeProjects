a2ff9bc84738273607abc2d54ca3f95c360e89b1
abuzarsince2002 <abuzarsince2002@gmail.com>
2024-12-18 14:12:43 +0530

    Emoji Dictionary first part commit.

---
+++ Emoji Dictionary/Emoji.swift
@@ -0,0 +1,14 @@
+//
+//  Emoji.swift
+//  Emoji Dictionary
+//
+//  Created by Abuzar Siddiqui on 15/12/24.
+//
+
+import Foundation
+struct Emoji{
+    var symbol: String
+    var name: String
+    var description: String
+    var usage: String
+}
